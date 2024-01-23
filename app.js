04d095850e7d28790a444762fa0d892832952eae
faraz <ranafaraznaru@gmail.com>
2024-01-23 22:54:16 +0500

    schemas,resolvers are added in app.js

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("hello world");
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery{
+        events:[String!]! 
+    }
+    type RootMutation{
+        createEvent(name:String):String
+    }
+    schema{
+        query:RootQuery
+        mutation:RootMutation
+    }
+    `),
+    // resolver
+    rootValue: {
+      events: () => {
+        return ["Ay", "Bee", "Cee", "Dee"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    // enable graphql playground
+    graphiql: true,
+  })
+);
 
 app.listen(3000);
